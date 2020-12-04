b73eaa03daca181507e4e4fa6f60dd74c7a45b83
Derviş Kopuz <derviskopuz@gmail.com>
2020-12-05 00:41:33 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ Date: <date> */
 using namespace std;
 struct Resistor{
   char group;
-  double value;
+  float value;
   int quantity;
   Resistor *next;
 };
@@ -21,9 +21,9 @@ struct Resistor{
 struct Circuit{
   Resistor *head;
   void create();
-  Resistor* search(char*, double);
-  void add_resistor(Circuit*, char, int);
-  int remove_resistor(char, int);
+  Resistor* search(char*, float);
+  void add_resistor(Circuit*, char, float);
+  int remove_resistor(char, float);
   void delete_resistor(char);
   void circuit_info();
   void clear();
@@ -33,7 +33,7 @@ void Circuit::create(){
   head = NULL;
 }
 
-Resistor* Circuit::search(char* group, double value) {
+Resistor* Circuit::search(char* group, float value) {
 
   Resistor* traverse;
   int counter = 0;
@@ -76,7 +76,7 @@ Resistor* Circuit::search(char* group, double value) {
   return NULL;
 }
 
-void Circuit::add_resistor(Circuit* circuit,char group, int value){
+void Circuit::add_resistor(Circuit* circuit,char group, float value){
   Resistor *newResistor = new Resistor;
   
   newResistor->group = group;
@@ -111,7 +111,7 @@ void Circuit::add_resistor(Circuit* circuit,char group, int value){
   //prev resistors next should point to newResistor
 }
 
-int Circuit::remove_resistor(char group, int value){
+int Circuit::remove_resistor(char group, float value){
 
   Resistor* ptr = Circuit::head;
   int needsDeletion = 0;
@@ -227,15 +227,7 @@ int main(){
   Circuit *circuit = new Circuit;
   circuit->create();
 
-/*
-  Resistor *resistor1 = new Resistor;
-  char testGroup[] = "A";
-  resistor1->group = *testGroup;
-  resistor1->value = 3.5;
-  resistor1->quantity = 1;
-  resistor1->next = NULL;
-  circuit->head = resistor1;
-  /*
+  
 
   /*fstream circuitInput;
   circuitInput.open("input.txt");
@@ -262,22 +254,22 @@ int main(){
   
   
   int i = 0;
-  char group[1];
-  double value;
+  char group;
+  float value;
   while (circuitFile >> group >> value)
   {
-    cout << group << " " << value << endl;
+    //cout << group << " " << value << endl;
     
     if (value > 0)
     {
       //traverse circuit by group
       //if group exists increase quantity by 1
-      Resistor* searchResponse = circuit->search(group, value);
+      Resistor* searchResponse = circuit->search(&group, value);
 
       //if group does not exist 
       if (searchResponse == NULL)
       {
-        circuit->add_resistor(circuit, *group, value);
+        circuit->add_resistor(circuit, group, value);
 
       } else{
         searchResponse->quantity++;
@@ -286,7 +278,7 @@ int main(){
 
     } else if (value < 0){
 
-      Resistor* searchResponse = circuit->search(group, value);
+      Resistor* searchResponse = circuit->search(&group, value);
       
       if (searchResponse == NULL)
       {
@@ -305,7 +297,7 @@ int main(){
       
       
 
-    } else if (*group == 'A' && value == 0)
+    } else if (group == 'A' && value == 0)
     {
       circuit->circuit_info();
     }
